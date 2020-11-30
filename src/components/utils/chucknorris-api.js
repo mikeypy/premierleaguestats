00ec11ec6912dbfefbcd70be89714af2c61bf690
mikeypy <michaelakins@hotmail.co.uk>
2020-11-30 11:53:15 +0000

    Update chucknorris-api.js

--- src/components/utils/chucknorris-api.js
+++ src/components/utils/chucknorris-api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const BASE_URL = 'https://api.football-data.org/';
 
 var config = {
-  headers: {'X-Auth-Token': '6e6322fc020e47c3b0ccac3c371a70a7'}
+  headers: {'X-Auth-Token': PREMIER_LEAGUE}
 };
 
 
